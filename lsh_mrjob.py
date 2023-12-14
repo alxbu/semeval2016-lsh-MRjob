656a22b3e9c84624992670349efa0a964c6b505d
Alexander Buls <alxbu.dev@gmail.com>
2023-12-14 10:57:53 +0100

    UPDATE lsh_mrjob.py

--- lsh_mrjob.py
+++ lsh_mrjob.py
@@ -5,6 +5,8 @@ import os
 import random
 
 os.environ['PYTHONHASHSEED'] = '0'
+HASHUNIVERSE = 2**32 - 1
+
 
 def hash_shingle(shingle):
     """
@@ -12,8 +14,16 @@ def hash_shingle(shingle):
     :param shingle:
     :return:
     """
-    HASHUNIVERSE = 2**32 - 1
     return int(hashlib.sha1(shingle.encode()).hexdigest(), 16) % HASHUNIVERSE
+
+
+def hash_band_signatures(signature):
+    """
+    Hashes the band signatures to a integer -> index of the bucket
+    :param signature: band signature
+    :return: index of the bucket
+    """
+    return int(hashlib.sha1(signature.encode()).hexdigest(), 16) % HASHUNIVERSE
 
 
 def universal_hashing(p, m):
@@ -28,10 +38,11 @@ def universal_hashing(p, m):
     return lambda x: ((a*x + b) % p) % m
 
 
-univ_hashes = [universal_hashing(2**32 - 5, 100) for _ in range(20)]
-
-
 class LocalitySensitiveHashing(MRJob):
+
+    def __init__(self, *args, **kwargs):
+        super(LocalitySensitiveHashing, self).__init__(*args, **kwargs)
+        self.univ_hashes = [universal_hashing(2**32 - 5, HASHUNIVERSE) for _ in range(self.options.num_hash_functions)]
 
     def configure_args(self):
         super(LocalitySensitiveHashing, self).configure_args()
@@ -42,12 +53,23 @@ class LocalitySensitiveHashing(MRJob):
         self.add_passthru_arg('--k', type=int, default=3, help="Length of shingles")
 
     def lines_to_tuples(self, _, line):
-        # yield each word in the line
+        """
+        Reads the data from the file and yields tuples of (hash, line)
+        :param _: key
+        :param line: A line from the file
+        :return: (hash, line) tuple
+        """
         line1, line2, hash1, hash2 = line.split('\t')
         yield hash1, line1
         yield hash2, line2
 
     def shingle_lines(self, doc_id, line):
+        """
+        Shingles the lines and yields tuples of (doc_id, shingle)
+        :param doc_id: document id
+        :param line: line from the file
+        :return: (doc_id, shingle) tuple
+        """
         if self.options.word_shingling:
             for inp in line:
                 words = inp.split()
@@ -58,9 +80,89 @@ class LocalitySensitiveHashing(MRJob):
                 for i in range(len(inp) - self.options.k + 1):
                     yield doc_id, inp[i:i + self.options.k]
 
+    def generate_signature(self, doc_id, shingles):
+        """
+        Generates the signature matrix
+        :param doc_id: document id
+        :param shingles: shingles of the document
+        :return: (doc_id, signature) tuple
+        """
+        for shingle in shingles:
+            if self.options.word_shingling:
+                shingle = ' '.join(shingle)
+            yield doc_id, hash_shingle(shingle)
+
+    def min_hash(self, doc_id, signatures):
+        """
+        Min hashes the signature matrix
+        :param doc_id: document id
+        :param signatures: signature matrix
+        :return: (doc_id, min_hash) tuple
+        """
+        min_hashes = [float('inf')] * self.options.num_hash_functions
+        for signature in signatures:
+            for i in range(self.options.num_hash_functions):
+                min_hashes[i] = min(min_hashes[i], self.univ_hashes[i](signature))
+        yield doc_id, min_hashes
+
+    def generate_bands(self, doc_id, min_hashes):
+        """
+        Hashes the min hashes to buckets
+        :param doc_id: document id
+        :param min_hashes: min hashes
+        :return: (doc_id, bucket) tuple
+        """
+        num_signatures_per_band = self.options.num_hash_functions // self.options.num_bands
+        min_hashes = [item for sublist in list(min_hashes) for item in sublist]
+        for i in range(self.options.num_bands):
+            yield i, (doc_id, min_hashes[i*num_signatures_per_band:(i+1)*num_signatures_per_band])
+
+    def locality_sensitive_hashing(self, band_id, docs):
+        """
+        Performs locality sensitive hashing
+        :param band_id: band id
+        :param docs: documents in the band
+        :return: None
+        """
+        for doc_id, signature in docs:
+            yield (band_id, hash_band_signatures(str(signature))), doc_id
+
+    def find_pairs_from_bucket(self, band_bucket, document_ids):
+        document_ids = list(document_ids)
+        if len(document_ids) > 1:
+            for i in range(len(document_ids)):
+                for j in range(i+1, len(document_ids)):
+                    yield (document_ids[i], document_ids[j]), 1
+
+    def combine_pairs(self, pairs, step_num=0):
+
+        pairs = list(pairs)
+        for sublist in pairs:
+            pair, count = sublist
+            yield None, pair
+
+    def final_reducer(self, _, pairs):
+        """
+        Final reducer to find the similar pairs
+        Each pair is emitted only once
+        Each pair counts as candidate
+        :param _: key
+        :param pairs: pairs of documents
+        :return: None
+        """
+        pairs = list(pairs)
+        for pair in pairs:
+            yield pair[0], pair[1]
+
     def steps(self):
         return [
-            MRStep(mapper=self.lines_to_tuples, reducer=self.shingle_lines)
+            MRStep(mapper=self.lines_to_tuples, reducer=self.shingle_lines),
+            MRStep(reducer=self.generate_signature),
+            MRStep(reducer=self.min_hash),
+            MRStep(reducer=self.generate_bands),
+            MRStep(reducer=self.locality_sensitive_hashing),
+            MRStep(reducer=self.find_pairs_from_bucket),
+            MRStep(combiner=self.combine_pairs, reducer=self.final_reducer)
         ]
 
 
